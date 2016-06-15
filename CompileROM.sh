aa4b2cdef7931bca83dfdf73207ce20b2bb0d80b
pirej <pirej30@yahoo.com>
2016-06-15 06:07:54 +0200

    using java 8 now

--- CompileROM.sh
+++ CompileROM.sh
@@ -2,10 +2,10 @@
 if [[ $# = 1 ]]; then
   . build/envsetup.sh
   if [[ $? = 0 ]]; then
-    # Use local Java Development Kit 7
-    if (( $(java -version 2>&1 | grep version | cut -f2 -d".") > 7 )); then
-       echo "Using local OpenJDK 7..."
-       export JAVA_HOME=$(realpath ../jdk1.7.0_65);
+    # Use local Java Development Kit 8
+    if (( $(java -version 2>&1 | grep version | cut -f2 -d".") > 8 )); then
+       echo "Using local OpenJDK 8..."
+       export JAVA_HOME=$(realpath ../jdk1.8.0_91);
     fi
     case $1 in
     -u)
